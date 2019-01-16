91c524c8294b8469ac0e18bcbd49cf6708732b40
chenqiang <ichenq@outlook.com>
2019-01-16 17:29:41 +0800

    Update example

--- examples/array-map/导出Go.bat
+++ examples/array-map/导出Go.bat
@@ -3,11 +3,13 @@ echo off
 set currentDir=%cd%
 cd ..\..\
 set rootDir=%cd%
+cd %currentDir%
 
+set taxi_alias=python %rootDir%\taxi\taxi.py
 set importArgs="file=%currentDir%\新手任务.xlsx"
 set exportArgs="outdata-dir=%currentDir%,out-src-file=%currentDir%/config.go,pkg=Config"
 
-python taxi\taxi.py  --mode=excel --import-args=%importArgs% --generator="go-v1" --export-args=%exportArgs%
+%taxi_alias%  --mode=excel --import-args=%importArgs% --generator="go-v1" --export-args=%exportArgs%
 
 pause
 REM array-delim
