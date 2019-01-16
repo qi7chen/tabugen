8ce6c4554eccc262fe4f1ed9faeaccbcd31e52ff
chenqiang <ichenq@outlook.com>
2019-01-16 17:29:58 +0800

    Update example

--- examples/basic/导出CSharp.bat
+++ examples/basic/导出CSharp.bat
@@ -3,10 +3,12 @@ echo off
 set currentDir=%cd%
 cd ..\..\
 set rootDir=%cd%
+cd %currentDir%
 
+set taxi_alias=python %rootDir%\taxi\taxi.py
 set importArgs="file=%currentDir%\±øÖÖ.xlsx"
 set exportArgs="outdata-dir=%currentDir%,out-src-file=%currentDir%/Config.cs,pkg=Config"
 
-python taxi\taxi.py  --mode=excel --import-args=%importArgs% --generator="cs-v1" --export-args=%exportArgs%
+%taxi_alias%  --mode=excel --import-args=%importArgs% --generator="cs-v1" --export-args=%exportArgs%
 
 pause
