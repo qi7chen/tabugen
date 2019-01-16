d2dd32338877e3eebf33676d80e361d9a4007b67
chenqiang <ichenq@outlook.com>
2019-01-16 17:30:46 +0800

    Update example

--- examples/sql/导出Go.bat
+++ examples/sql/导出Go.bat
@@ -3,11 +3,13 @@ echo off
 set currentDir=%cd%
 cd ..\..\
 set rootDir=%cd%
+cd %currentDir%
 
+set taxi_alias=python %rootDir%\taxi\taxi.py
 set importArgs="user=root,passwd=holyshit,db=test,table=player"
 set exportArgs="outdata-dir=%currentDir%,out-src-file=%currentDir%/config.go,pkg=orm"
 
-python taxi\taxi.py  --mode=mysql --import-args=%importArgs% --generator="go-v2" --export-args=%exportArgs%
+%taxi_alias%   --mode=mysql --import-args=%importArgs% --generator="go-v2" --export-args=%exportArgs%
 
 pause
 REM array-delim
