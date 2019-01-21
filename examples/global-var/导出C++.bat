51057ac7858866d7b73d1bf9d1b025d07d1d6295
chenqiang <ichenq@outlook.com>
2019-01-21 10:27:42 +0800

    Move dir

--- examples/global-var/导出C++.bat
+++ examples/global-var/导出C++.bat
@@ -7,7 +7,7 @@ cd %currentDir%
 
 set taxi_alias=python %rootDir%\taxi\taxi.py
 set importArgs="file=%currentDir%\全局变量表.xlsx"
-set exportArgs="outdata-dir=%currentDir%,out-src-file=%currentDir%/AutoGenConfig,pkg=config"
+set exportArgs="outdata-dir=%currentDir%\proj,out-src-file=%currentDir%\proj\AutoGenConfig,pkg=config"
 
 %taxi_alias%  --mode=excel --import-args=%importArgs% --generator="cpp-v1" --export-args=%exportArgs%
 
