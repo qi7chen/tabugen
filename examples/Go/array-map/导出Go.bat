38d0b523416fa0119a4878a9663895f041de1c4a
chenqiang <ichenq@outlook.com>
2019-03-29 02:04:45 +0800

    refactor examples

---
+++ examples/Go/array-map/导出Go.bat
@@ -0,0 +1,16 @@
+echo off
+
+set currentDir=%cd%
+cd ..\..\
+set rootDir=%cd%
+cd %currentDir%
+set PYTHONPATH=%rootDir%
+
+set taxi_alias=python %rootDir%\taxi\cli.py
+set importArgs="file=%currentDir%\新手任务.xlsx"
+set exportArgs="pkg=config,outdata-dir=%currentDir%\proj,out-src-file=%currentDir%\proj\autogen.go"
+
+%taxi_alias%  --mode=excel --import-args=%importArgs% --generator="go-v1" --export-args=%exportArgs%
+
+pause
+REM array-delim
---
+++ examples/Go/sql/导出Go.bat
@@ -0,0 +1,16 @@
+echo off
+
+set currentDir=%cd%
+cd ..\..\
+set rootDir=%cd%
+cd %currentDir%
+set PYTHONPATH=%rootDir%
+
+set taxi_alias=python %rootDir%\taxi\cli.py
+set importArgs="user=root,passwd=holyshit,db=test,table=player"
+set exportArgs="outdata-dir=%currentDir%,out-src-file=%currentDir%/config.go,pkg=orm"
+
+%taxi_alias%   --mode=mysql --import-args=%importArgs% --generator="go-v2" --export-args=%exportArgs%
+
+pause
+REM array-delim
