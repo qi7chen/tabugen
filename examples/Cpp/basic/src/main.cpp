880f1de35fde6f2664221328f5c4fda619d63a19
chenqiang <ichenq@outlook.com>
2019-05-11 15:19:43 +0400

    Update cpp example

--- examples/Cpp/basic/src/main.cpp
+++ examples/Cpp/basic/src/main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <type_traits>
 #include <iostream>
-#include "AutoGenConfig.h"
+#include <fstream>
+#include "AutogenConfig.h"
 #include "Utility/StringUtil.h"
 
+using namespace std;
 
 static std::string readfile(const char* filepath)
 {
@@ -11,14 +13,21 @@ static std::string readfile(const char* filepath)
     std::ifstream ifs(filename.c_str());
     std::string content((std::istreambuf_iterator<char>(ifs)),
         (std::istreambuf_iterator<char>()));
+    //cout << (void*)content.data() << endl;
+    //cout << content << endl;
     return std::move(content);
 }
 
 int main(int argc, char* argv[])
 {
-    using namespace config;
+    using namespace autogen;
     AutogenConfigManager::reader = readfile;
     AutogenConfigManager::LoadAll();
-    std::cout << "FreeCompleteSeconds: " << GlobalPropertyDefine::Instance()->FreeCompleteSeconds << std::endl;
+    const std::vector<SoldierPropertyDefine>* all = SoldierPropertyDefine::GetData();
+    cout << stringPrintf("%d soldier config loaded", (int)all->size());
+    for (const SoldierPropertyDefine& item : *all)
+    {
+        cout << stringPrintf("%s %d", item.Name.c_str(), (int)item.Level) << endl;
+    }
     return 0;
 }
