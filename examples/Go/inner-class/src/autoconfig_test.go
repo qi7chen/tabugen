ec09e0c05092b87e1e0fc9c589fccf00cf483827
chenqiang <ichenq@outlook.com>
2019-05-11 22:29:31 +0400

    update go example

---
+++ examples/Go/inner-class/src/autoconfig_test.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestAutogenConfig(t *testing.T) {
+	var loader = NewFileLoader("../res")
+	defer loader.Close()
+
+	conflist, err := LoadBoxProbabilityDefineList(loader)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	for _, cfg := range conflist {
+		t.Logf("%v\n", cfg)
+	}
+}
