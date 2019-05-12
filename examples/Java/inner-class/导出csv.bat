84ee6fdb2849d4d8b208de6b8106820ab4ea50e5
chenqiang <ichenq@outlook.com>
2019-05-12 21:01:28 +0400

    update java example

---
+++ examples/Java/inner-class/导出csv.bat
@@ -0,0 +1,16 @@
+echo off
+
+set currentDir=%cd%
+cd ..\..\..\
+set rootDir=%cd%
+cd %currentDir%
+
+set PYTHONPATH=%rootDir%
+
+set taxi_alias=python %rootDir%\taxi\cli.py
+set importArgs="file=%currentDir%\Ëæ»ú±¦Ïä.xlsx"
+set exportArgs="pkg=com.mycompany.csvconfig,outdata-dir=%currentDir%\proj\src\main\resources,out-src-file=%currentDir%\proj\src\main\java"
+
+%taxi_alias%  --mode=excel --import-args=%importArgs% --generator="java-csv" --output-format=csv --export-args=%exportArgs%
+
+pause
