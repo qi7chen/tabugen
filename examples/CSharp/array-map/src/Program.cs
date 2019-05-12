5422170fc07749d65811325d6c25131490d537cb
chenqiang <ichenq@outlook.com>
2019-05-12 09:34:37 +0400

    update C# example

---
+++ examples/CSharp/array-map/src/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+#if UNITY 
+using UnityEngine;
+using UnityEngine.Networking;
+#endif
+
+namespace CSharpDemo
+{
+    class Program
+    {
+#if UNITY        
+        public static void GetStreamingContent(string path, Action<string> cb)
+        {
+            string filePath = Path.Combine(Application.streamingAssetsPath, path);
+    #if UNITY_ANDROID
+            StartCoroutine(LoadAsset(filePath, cb));
+    #else
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                cb(reader.ReadToEnd());
+            }
+    #endif
+        }
+
+        IEnumerator LoadAsset(string filePath, Action<string> cb)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get(filePath))
+            {
+                yield return www.SendWebRequest();
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.LogErrorFormat("LoadAsset: error: {0} {1}",  www.error, filePath);
+                    cb(null);
+                }
+                else
+                {
+                    cb(www.downloadHandler.data);
+                }
+            }
+        }
+#endif         
+    
+        static void ReadFileContent(string filepath, Action<string> cb)
+        {
+#if UNITY
+            GetStreamingContent(filepath, cb);
+#else
+            string path = string.Format("../../../../res/{0}", filepath);
+            StreamReader reader = new StreamReader(path);
+            var content = reader.ReadToEnd();
+            cb(content);    
+#endif
+        }
+
+        static void onLoaded()
+        {
+            foreach (var item in Config.NewbieGuideDefine.Data)
+            {
+                Console.WriteLine(string.Format("{0} {1} {2}", item.Name, item.Type, item.Accomplishment));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Config.AutogenConfigManager.reader = ReadFileContent;
+                Config.AutogenConfigManager.LoadAllConfig(() =>
+                {
+                    Console.WriteLine("OK");
+                    onLoaded();
+                });
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ examples/CSharp/array-map/导出json.bat
@@ -0,0 +1,16 @@
+echo off
+
+set currentDir=%cd%
+cd ..\..\..\
+set rootDir=%cd%
+cd %currentDir%
+set PYTHONPATH=%rootDir%
+
+set taxi_alias=python %rootDir%\taxi\cli.py
+set importArgs="file=%currentDir%\res\新手任务.xlsx"
+set exportArgs="pkg=AutoConfig,outdata-dir=%currentDir%\res,out-src-file=%currentDir%\src\AutoJsonConfig.cs"
+
+%taxi_alias%  --mode=excel --import-args=%importArgs% --generator="cs-json" --output-format=json --export-args=%exportArgs%
+
+pause
+REM array-delim
