e53ab86dbdbed563b5740b718361e38418744fe5
chenqiang <qiang.chen@igg.com>
2021-04-12 13:53:31 +0800

    bump version

--- tabular/version.py
+++ tabular/version.py
@@ -2,8 +2,8 @@
 # Distributed under the terms and conditions of the Apache License.
 # See accompanying files LICENSE.
 
-_major = 1
-_minor = 4
+_major = 0
+_minor = 5
 _patch = 0
 
 VER_STRING = '%d.%d.%d' % (_major, _minor, _patch)
