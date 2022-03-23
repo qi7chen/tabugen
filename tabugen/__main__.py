6bd5e212656d9ee5f7a3e9df9b1bda25af7c77d8
simon <simon@qchen.fun>
2022-03-23 09:36:19 +0800

    add main entry

---
+++ tabugen/__main__.py
@@ -0,0 +1,14 @@
+"""The main entry point. Invoke as `tabugen' or `python -m tabugen'.
+"""
+
+
+def main():
+    try:
+        from tabugen.cli import main
+        main()
+    except KeyboardInterrupt:
+        return
+
+
+if __name__ == '__main__':
+    main()
