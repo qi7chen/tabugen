bb7a6b3f438ba5d04d9b8b9e3b476a15daaefab8
qiangchen <ichenq@outlook.com>
2023-08-19 22:14:00 +0800

    update

--- setup.py
+++ setup.py
@@ -11,24 +11,24 @@ sys.path.append(this_directory)
 with open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf-8') as fh:
     readme = fh.read()
 
-tabugen_version = '0.9.1'
+tabugen_version = '1.0.1'
 
 install_requires = [
     'xlrd>=2.0.1'
-    'openpyxl>=3.0.10',
+    'openpyxl>=3.1.2',
 ]
 
 setuptools.setup(
     name='tabugen',
     version=tabugen_version,
     license='Apache License 2.0',
-    author='qchencc',
+    author='JohnnieChen',
     author_email='ichenq@outlook.com',
-    url='https://github.com/qchencc/tabugen',
+    url='https://github.com/qki7chen/tabugen',
     description=u'a spreedsheet export and code generate tool for rapid game development',
     long_description=readme,
     long_description_content_type='text/markdown',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=install_requires,
--- tabugen/version.py
+++ tabugen/version.py
@@ -4,8 +4,8 @@ Distributed under the terms and conditions of the Apache License.
 See accompanying files LICENSE.
 """
 
-_major = 0
-_minor = 10
-_patch = 0
+_major = 1
+_minor = 0
+_patch = 1
 
 VER_STRING = '%d.%d.%d' % (_major, _minor, _patch)
